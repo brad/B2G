39b8c45a559b618b25d220d6ac5769469a214566
Luis Cruz <ljc2491@gmail.com>
2012-09-27 01:22:31 -0500

    config.sh: remove the need to extract vendor blobs Our vendor blobs are brough together using repo sync. No need to connect your device :p

--- config.sh
+++ config.sh
@@ -85,8 +85,7 @@ case "$1" in
 
 "glacier")
 	echo DEVICE=glacier >> .tmp-config &&
-	repo_sync glacier &&
-	(cd device/htc/glacier && ./download-blobs.sh)
+	repo_sync glacier
 	;;
 
 "otoro_m4-demo")
